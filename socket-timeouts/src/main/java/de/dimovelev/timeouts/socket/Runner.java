bb3cdd556e6fbe30e955770a5bf84d16d0446e64
Dimo Velev <dimo.velev@gmail.com>
2015-03-22 08:50:07 +0100

    retry the read multiple times

--- socket-timeouts/src/main/java/de/dimovelev/timeouts/socket/Runner.java
+++ socket-timeouts/src/main/java/de/dimovelev/timeouts/socket/Runner.java
@@ -8,14 +8,15 @@ import java.net.*;
 
 public class Runner {
     public static void main(String[] args) throws Exception {
-        if (args.length != 4) {
-            System.out.println("Usage: java -jar socket-timeouts*.jar <host> <port> <connect-timeout> <so-timeout>");
+        if (args.length != 5) {
+            System.out.println("Usage: java -jar socket-timeouts*.jar <host> <port> <connect-timeout> <so-timeout> <read-retries>");
             System.exit(1);
         }
         final String host = args[0];
         final int port = Integer.parseInt(args[1]);
         final int connectTimeout = Integer.parseInt(args[2]);
         final int soTimeout = Integer.parseInt(args[3]);
+        final int readRetries = Integer.parseInt(args[4]);
         final Socket socket = new Socket();
         if (soTimeout > 0) {
             socket.setSoTimeout(soTimeout);
@@ -29,7 +30,14 @@ public class Runner {
         out.write("Hello\n");
         out.flush();
         final BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        System.out.println(in.readLine());
+        for (int i=0; i<readRetries; i++) {
+            try {
+                System.out.println(in.readLine());
+                break;
+            } catch (SocketTimeoutException e) {
+                System.out.println(e.getClass() + ": " + e.getMessage());
+            }
+        }
         socket.close();
     }
 }
